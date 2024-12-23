4f8b099a87725b0092e186b776ca7d1ae8525697
ynwin1 <ynwinubc24@gmail.com>
2024-12-22 19:43:18 -0800

    adjust footer z-index

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <footer className="relative bg-transparent text-white p-4 flex flex-row gap-6 justify-center items-center bottom-0 z-50">
+        <footer className="relative bg-transparent text-white p-4 flex flex-row gap-6 justify-center items-center bottom-0">
             {/*<div className="flex flex-row justify-evenly w-full">*/}
             {/*    <Link href="/">*/}
             {/*        <p className="text-2xl font-bold hover:opacity-60">Home</p>*/}
