2e31d7fd88c4eb08952a04c4566e64d856ee9161
ynwin1 <ynwinubc24@gmail.com>
2024-12-23 15:10:16 -0800

    button adjustments

--- app/components/language-switch/LanguageSwitcher.tsx
+++ app/components/language-switch/LanguageSwitcher.tsx
@@ -23,13 +23,13 @@ const LanguageSwitcher = () => {
     return (
         <div className="inline-flex rounded-md shadow-sm" role="group">
             <button type="button"
-                    className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
+                    className="px-5 py-2.5 text-lg font-medium text-gray-900 bg-transparent border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
                     onClick={() => switchLanguage('en')}
             >
                 English
             </button>
             <button type="button"
-                    className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
+                    className="px-5 py-2.5 text-lg font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
                     onClick={() => switchLanguage('mm')}
             >
                 မြန်မာ
--- app/components/navbar/menu/Menu.tsx
+++ app/components/navbar/menu/Menu.tsx
@@ -45,7 +45,7 @@ const MenuOverlay = ({ close, locale }: { close: React.Dispatch<React.SetStateAc
                     <HomeIcon className={"w-8"} />
                 </Link>
                 <LanguageSwitcher/>
-                    {/* TODO - Request Song */}
+                {/* https://flowbite.com/docs/components/buttons/ */}
                 <button
                     className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
                     <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
