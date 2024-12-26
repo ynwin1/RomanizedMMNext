bd625f9c0dd8b9ca0c78cb61e40365c707a4a9b4
ynwin1 <ynwinubc24@gmail.com>
2024-12-26 10:57:53 -0800

    read GTL page

---
+++ app/(pages)/[locale]/(songs)/guess-the-lyrics/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Song from "@/app/model/Song";
+import Trivia from "@/app/components/guess-the-lyrics/Trivia";
+import Player from "@/app/components/video-player/Player";
+import connectDB from "@/app/lib/mongodb";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Guess The Lyrics',
+    description: 'Test your knowledge of Myanmar songs by guessing the lyrics of the songs!',
+};
+
+const Page = async () => {
+    let allSongs = [];
+    try {
+        await connectDB();
+        allSongs = await Song.find({}).select("songName romanized burmese -_id").lean();
+    } catch (e) {
+        console.error(e);
+        throw new Error("Failed to fetch songs in GuessTheLyrics. Please try again later.");
+    }
+
+    return (
+        <main className="flex flex-col justify-center items-center gap-6 min-h-screen">
+            <div className="fixed inset-0 w-full h-full">
+                <Player src="/RMBG.mp4" />
+            </div>
+            <Trivia songs={allSongs} />
+        </main>
+    )
+}
+export default Page
--- app/components/guess-the-lyrics/Trivia.tsx
+++ app/components/guess-the-lyrics/Trivia.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {Suspense, useState} from 'react'
+import React, {useState} from 'react'
 import {useTranslations} from "next-intl";
 import {TriviaScoreForm} from "@/app/components/forms/TriviaScoreForm";
 import Leaderboard from "@/app/components/guess-the-lyrics/Leaderboard";
