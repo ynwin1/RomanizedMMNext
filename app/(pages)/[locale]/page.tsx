e8ec42169c46c1c5cbc937a559cac4c7d70165af
ynwin1 <ynwinubc24@gmail.com>
2024-12-26 19:47:26 -0800

    no translation for home page

--- app/(pages)/[locale]/page.tsx
+++ app/(pages)/[locale]/page.tsx
@@ -3,10 +3,8 @@ import Player from "@/app/components/video-player/Player";
 import {Navbar} from "@/app/components/navbar/Navbar";
 import Footer from "@/app/components/footer/Footer";
 import {lusitana} from "@/app/components/fonts/fonts";
-import {useTranslations} from "next-intl";
 import {setRequestLocale} from "next-intl/server";
-import AboutContent from "@/app/components/about/AboutContent";
-import {Suspense} from "react";
+import React from "react";
 
 interface HomeProps {
     params: Promise<{locale: string}>;
@@ -16,7 +14,6 @@ export default async function Home({params}: HomeProps) {
     const {locale} = await params;
     // Enable static rendering
     setRequestLocale(locale);
-
 
     return (
         <div className={`min-h-screen flex flex-col ${lusitana.className} antialiased`}>
@@ -30,7 +27,12 @@ export default async function Home({params}: HomeProps) {
             {/* Main content */}
             <main className="flex-1 flex flex-col">
                 <div className="relative z-10 flex flex-col items-center justify-center flex-1 gap-6 text-center px-4">
-                    <AboutContent />
+                    <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold">
+                        Sing Myanmar, Globally!
+                    </h2>
+                    <h4 className="text-lg md:text-2xl lg:text-3xl font-medium">
+                        Discover romanized lyrics of your favorite Myanmar songs
+                    </h4>
                     <SearchBar />
                 </div>
             </main>
