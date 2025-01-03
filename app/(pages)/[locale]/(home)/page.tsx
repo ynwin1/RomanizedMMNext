078d16bcd7ef7d4dbf9d161f95dc83e425f85911
ynwin1 <ynwinubc24@gmail.com>
2025-01-03 11:30:38 -0800

    metadata for home page

--- app/(pages)/[locale]/(home)/page.tsx
+++ app/(pages)/[locale]/(home)/page.tsx
@@ -5,6 +5,33 @@ import Footer from "@/app/components/footer/Footer";
 import {lusitana} from "@/app/components/fonts/fonts";
 import {setRequestLocale} from "next-intl/server";
 import React from "react";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Home",
+    description: "Welcome to RomanizedMM! Discover the lyrics of your favorite Myanmar songs here!",
+    keywords: "Myanmar, song, lyrics, Romanized, Burmese, music, romanization, Myanmar songs, Myanmar music, Myanmar lyrics",
+    category: "Music",
+    creator: "RomanizedMM",
+    publisher: "RomanizedMM",
+    openGraph: {
+        title: "RomanizedMM",
+        description: "Discover the lyrics of your favorite Myanmar songs and sing along!",
+        type: "website",
+        url: "https://romanizedmm.com",
+        siteName: "RomanizedMM",
+        images: [
+            {
+                url: "https://i.imgur.com/YqMt5kd.png",
+                width: 800,
+                height: 600,
+                alt: "RomanizedMM",
+            },
+        ],
+        alternateLocale: ["en", "my"],
+        locale: "en",
+    }
+}
 
 interface HomeProps {
     params: Promise<{locale: string}>;
