6c1e5bee41dff2729747a59f533e28d5a87d23ca
ynwin1 <ynwinubc24@gmail.com>
2025-03-27 18:06:45 -0700

    Artist model

---
+++ app/model/Artist.ts
@@ -0,0 +1,49 @@
+import mongoose, { Schema, Model, models } from "mongoose";
+
+export interface IArtist extends mongoose.Document {
+    name: string;
+    slug: string;
+    imageLink?: string;
+    bannerLink?: string;
+    biography: string;
+    type: "Singer" | "Band";
+    members?: string[];
+    origin?: string;
+    labels?: string[];
+    musicGenre: string[];
+    songs: number[];
+    socials?: {
+        facebook?: string;
+        instagram?: string;
+        youtube?: string;
+        spotify?: string;
+        appleMusic?: string;
+    },
+    likes: number;
+}
+
+const ArtistSchema: Schema<IArtist> = new Schema({
+    name: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    imageLink: { type: String },
+    bannerLink: { type: String },
+    biography: { type: String, required: true },
+    type: { type: String, required: true },
+    members: { type: [String] },
+    origin: { type: String },
+    labels: { type: [String] },
+    musicGenre: { type: [String], required: true },
+    songs: { type: [Number], required: true },
+    socials: {
+        facebook: { type: String },
+        instagram: { type: String },
+        youtube: { type: String },
+        spotify: { type: String },
+        appleMusic: { type: String },
+    },
+    likes: { type: Number, default: 0 },
+});
+
+const Artist: Model<IArtist> = models.Artist || mongoose.model("Artist", ArtistSchema);
+
+export default Artist;
