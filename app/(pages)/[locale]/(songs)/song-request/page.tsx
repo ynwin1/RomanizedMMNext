de940f74be999c3deb5bb37db2f65f29acc8504b
ynwin1 <ynwinubc24@gmail.com>
2025-04-15 23:40:51 -0700

    status indicator in song request

--- app/(pages)/[locale]/(songs)/song-request/page.tsx
+++ app/(pages)/[locale]/(songs)/song-request/page.tsx
@@ -23,12 +23,21 @@ const Page = async () => {
             </p>
             <SongRequestForm/>
 
+            {/* Status */}
+            <div className="flex flex-row items-center justify-center border-2 border-white rounded-2xl">
+                <label className="text-lg font-bold p-3">Status:</label>
+                <div className="flex flex-row items-center gap-x-2 p-3">
+                    <p className="text-base">Busy</p>
+                    <div className="w-4 h-4 bg-red-600 rounded-full"/>
+                </div>
+            </div>
+
             {/* Song Queue */}
             <div className="flex flex-col text-center justify-center items-center w-[60vw] max-md:w-[80vw] border-2 border-white p-4 rounded-2xl mb-6">
                 <p className="text-2xl font-bold mt-2 mb-6 max-md:text-lg">{translator("songsInQueue")}</p>
                 {songRequests.map((songRequest, index) => {
                     return (
-                        <div key={index} className="flex flex-row justify-center bg-white mb-6 rounded-2xl p-3 text-black w-[40vw] max-md:w-[60vw]">
+                        <div key={index} className="flex flex-row justify-center bg-white mb-6 rounded-2xl p-3 text-black w-[40vw] max-md:w-[70vw]">
                             <p className="text-base">{`${songRequest.songName} - ${songRequest.artist}`}</p>
                         </div>
                     );
