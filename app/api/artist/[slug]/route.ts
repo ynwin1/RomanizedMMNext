ddb06f12de9ce8181fd18b0c0b2f9b786d27c90f
ynwin1 <ynwinubc24@gmail.com>
2025-05-13 23:38:41 -0700

    use context object as second param to GET API

--- app/api/artist/[slug]/route.ts
+++ app/api/artist/[slug]/route.ts
@@ -1,16 +1,21 @@
 import Artist from "@/app/model/Artist";
 import connectDB from "@/app/lib/mongodb";
-export async function GET(req: Request, { params }: { params: { slug: string } }) {
-    // fetch artist from db
-    try {
-        await connectDB();
-        const { slug } = params;
-        const artist = await Artist.findOne({ slug }).select("-_id -__v").lean();
-        if (!artist) {
-            return Response.json({ error: 'Artist not found' }, { status: 404 });
-        }
-        return Response.json(artist, { status: 200 });
-    } catch (error) {
-        return Response.json({ error: `Failed to fetch artist with error - ${error}` }, { status: 500 });
+import { NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  context: { params: { slug: string } }
+) {
+  // fetch artist from db
+  try {
+    await connectDB();
+    const { slug } = context.params;
+    const artist = await Artist.findOne({ slug }).select("-_id -__v").lean();
+    if (!artist) {
+      return Response.json({ error: 'Artist not found' }, { status: 404 });
     }
+    return Response.json(artist, { status: 200 });
+  } catch (error) {
+    return Response.json({ error: `Failed to fetch artist with error - ${error}` }, { status: 500 });
+  }
 }
