6a398ce929998a63982a8be14af4f230410c5050
ynwin1 <ynwinubc24@gmail.com>
2025-10-12 19:24:18 -0700

    Added roles and protected routing for admin role

---
+++ app/(pages)/[locale]/(admin)/admin/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function AdminDashboard() {
+    return <p>This is the protected admin dashboard restricted to users with the `admin` role.</p>
+}
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,22 @@
 import {clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server';
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
+import {NextResponse} from "next/server";
+import {Roles} from "@/types/globals";
 
 const intlMiddleware = createMiddleware(routing);
 
-const isProtectedRoute = createRouteMatcher(['/admin(.*)']);
+const isAdminRoute = createRouteMatcher(['/(en|my)?/admin(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) {
-        await auth.protect((has) => {
-            return has({ permission: 'org:admin:example1'});
-        })
+    const role: Roles | undefined = (await auth()).sessionClaims?.metadata?.role;
+
+    const isAdminPath = isAdminRoute(req);
+    const isAdminRole = role === 'admin';
+    if (isAdminPath && !isAdminRole) {
+        console.log('Unauthorized access to admin route');
+        const url = new URL('/', req.url)
+        return NextResponse.redirect(url)
     }
 
     return intlMiddleware(req);
@@ -20,7 +26,9 @@ export const config = {
     matcher: [
         '/',
         '/(en|my)/:path*',
+        // Skip Next.js internals and all static files, unless found in search params
         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
         '/(api|trpc)(.*)',
     ]
 };
